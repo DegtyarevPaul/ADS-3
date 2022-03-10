735be15794c87a75a89bd37efe3714d97cd04357
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-03-11 00:14:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,27 @@
 // Copyright 2021 Degtyarev Paul
 
 int cbinsearch(int *arr, int size, int value) {
-int h = size - 1;
-int l = 0;
-int mid;
-int k = 1;
-while (l <= size) {
-mid = (l + h) / 2;
-if (value == *(arr + mid)) {
-while (mid < size - 1 && (*(arr + mid) >= *(arr + mid + 1))) {
-mid++;
-}
-while ((mid > 0) && (*(arr + mid) <= *(arr + mid - 1))) {
-k++;
-mid--;
-}
-return k;
-}
-else if (value > *(arr + mid)) {
-l = mid + 1;
-}
-else if (value < *(arr + mid)) {
-h = mid - 1;
-}
-}
-return 0;
+  int h = size - 1;
+  int l = 0;
+  int mid;
+  while (l <= size) {
+    mid = (l + h) / 2;
+    if (value == *(arr + mid)) {
+      l = h = mid;
+      while (*(arr + h) == *(arr + h + 1)) {
+        h++;
+      }
+      while (*(arr + l) == *(arr + l - 1)) {
+        l--;
+      }
+      return h - l + 1;
+    }
+    else if (value > *(arr + mid)) {
+      l = mid + 1;
+    }
+    else if (value < *(arr + mid)) {
+      h = mid - 1;
+    }
+  }
+  return 0;
 }
