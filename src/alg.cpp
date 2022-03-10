448750a5ffd528f3baa519886cc388d1bd6a333b
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-03-10 20:19:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,34 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2021 Degtyarev Paul
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int h = size - 1;
+int l = 0;
+int mid;
+int k = 1;
+while (l <= size)
+{
+mid = (l + h) / 2;
+if (value == *(arr + mid))
+{
+while (mid < size - 1 && (*(arr + mid) >= *(arr + mid + 1)))
+{
+mid++;
+}
+while ((mid > 0) && (*(arr + mid) <= *(arr + mid - 1)))
+{
+k++;
+mid--;
+}
+return k;
+}
+else if (value > *(arr + mid))
+{
+l = mid + 1;
+}
+else if (value < *(arr + mid))
+{
+h = mid - 1;
+}
+}
+return 0;
 }
